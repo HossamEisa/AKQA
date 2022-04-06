9a0a4286866ca23e44d6fa9e0eaadfca19a5880e
HossamEisa <39234961+HossamEisa@users.noreply.github.com>
2022-04-06 22:12:27 +0200

    fix scrolling issue

--- src/js/scroll-animation.js
+++ src/js/scroll-animation.js
@@ -27,7 +27,7 @@ const hideScrollElement = (element) => {
 
 const handleScrollAnimation = () => {
     scrollElements.forEach((el) => {
-        if (elementInView(el, 1.25)) {
+        if (elementInView(el, 1)) {
             displayScrollElement(el);
         }
         //comment this else if you want animation run once
